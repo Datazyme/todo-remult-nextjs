f702803b197b330a5c0ace0f0c4b468d86cb5e3b
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2025-02-09 21:59:08 -0800

    index.tsx calls on Tasks setstate with tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { remult } from "remult"
+import { Task } from "@/shared/Task"
+import { useEffect, useState } from "react"
+
+//call repository of tasks, covers front and back end
+const taskRepo = remult.repo(Task)
+
+//find gets all the tasks from backend
+function fetchTasks() {
+  return taskRepo.find()
+}
 
 export default function Home() {
+  const [tasks, setTasks]=useState<Task[]>([])
+
+  useEffect(() => {
+    fetchTasks().then(setTasks)
+  })
   return (
-    <>
-    </>
+    <div>
+      <h1>To Do</h1>
+    </div>
   )
 }
