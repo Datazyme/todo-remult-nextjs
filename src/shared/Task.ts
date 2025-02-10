c3207c32263e20ee459ad546695f38365a56d09f
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2025-02-09 21:46:25 -0800

    maybe add the right files

---
+++ src/shared/Task.ts
@@ -0,0 +1,17 @@
+import { Entity, Fields } from "remult"
+
+
+//To initially allow all CRUD operations for tasks, we set the option allowApiCrud to true.
+@Entity("tasks", {
+    allowApiCrud: true
+})
+
+//What is an entity https://remult.dev/tutorials/react/entities. Now need to register with remult object in file [...remult].ts
+export class Task {
+    @Fields.autoIncrement()
+    id = 0
+    @Fields.string()
+    title = ''
+    @Fields.boolean()
+    completed = false
+}
