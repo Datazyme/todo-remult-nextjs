3fc272070f839599c16d7865801c7e78f0fa76cc
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2025-03-02 14:05:02 -0800

    changed database url to transaction pooler

--- src/pages/api/[...remult].ts
+++ src/pages/api/[...remult].ts
@@ -2,10 +2,12 @@ import { remultNext } from "remult/remult-next";
 import { Task } from "@/shared/Task";
 import { TasksController } from "@/shared/TasksController";
 import { getUserFromNextAuth } from "./auth/[...nextauth]";
+import { createPostgresConnection } from "remult/postgres"
 
 export default remultNext({
     //this is the registering of completeAllTasks to ruemult
     controllers: [TasksController],
     entities: [Task],
-    getUser: getUserFromNextAuth
+    getUser: getUserFromNextAuth,
+    dataProvider: createPostgresConnection()
 })
